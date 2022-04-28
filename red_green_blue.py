2693ef88c6af4c6095ecb464c4345f996aca641c
Aidan-Lalonde-Novales <ubuntu@ip-172-31-55-218.ec2.internal>
2022-04-28 12:20:36 +0000

    added py file

---
+++ red_green_blue.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by Aidan Lalonde-Novales
+# Created April 2022
+# This program prints all possible RGB values.
+
+
+def main():
+    # this function prints all possible RGB values.
+
+    # process (kinda), input
+    red = 0
+    green = 0
+    blue = 0
+
+    for red in range(256):
+        for green in range(256):
+            for blue in range(256):
+                print("RGB({0}, {1}, {2})".format(red, green, blue))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
